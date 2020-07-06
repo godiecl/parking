fcf18a7e1d21ff536a779a37eb895130dfd0e6da
Diego Urrutia-Astorga <mika.cl@gmail.com>
2020-07-06 17:36:26 -0400

    feat(Json): add the implementation with lombok/json.

--- scraper/src/main/java/cl/ucn/disc/pdis/scraper/Main.java
+++ scraper/src/main/java/cl/ucn/disc/pdis/scraper/Main.java
@@ -24,27 +24,30 @@
 
 package cl.ucn.disc.pdis.scraper;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.Nullable;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The Class.
  *
  * @author Diego Urrutia-Astorga.
  */
+@Slf4j
 public final class Main {
-
-    /**
-     * The Logger.
-     */
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     /**
      * The entry point.
@@ -52,41 +55,112 @@ public final class Main {
      * @param args to use.
      * @throws IOException if any error.
      */
+    @SuppressWarnings("LawOfDemeter")
     public static void main(String[] args) throws IOException {
 
         // from .. to ..
-        final int ini = 1;
-        final int end = 1; // 40000;
+        final int ini = 21;
+        final int end = 40; // 40000;
+
+        // Contenedor of Personas
+        final List<Persona> personas = new ArrayList<>(100);
 
         log.debug("Getting the data from {} to {} ...", ini, end);
         for (int id = ini; id <= end; id++) {
 
-            // Just wait
-            try {
-                Thread.sleep(1000 + RandomUtils.nextInt(0, 1001));
-            } catch (InterruptedException e) {
-                // Nothing here
-            }
+            log.debug("Testing for id {} ...", id);
 
-            Document document = Jsoup.connect(
+            // Just wait
+            sleep();
+
+            // The HTML document
+            final Document document = Jsoup.connect(
                     "http://online.ucn.cl/directoriotelefonicoemail/fichaGenerica/?cod=" + id)
                     .get();
 
-            log.debug("Title: {}", document.title());
-
-            Element lblNombre = document.getElementById("lblNombre");
-            if (lblNombre == null || StringUtils.isEmpty(lblNombre.text())) {
-                log.warn("Can't find lblNombre in id {}, skipping ..", id);
+            // Trying to get the nombre
+            final String nombre = getText(document, "lblNombre");
+            if (nombre == null) {
+                log.debug("Name not found in id {}, skipping !!", id);
+                // Not found, skipping!
                 continue;
             }
-            log.debug("lblNombre: [{}].", lblNombre.text());
 
-            Element lblCargo = document.getElementById("lblCargo");
-            log.debug("lblCargo: [{}].", lblCargo.text());
+            // All the data ..
+            final String cargo = getText(document, "lblCargo");
+            final String unidad = getText(document, "lblUnidad");
+            final String email = getText(document, "lblEmail");
+            final String telefono = getText(document, "lblTelefono");
+            final String oficina = getText(document, "lblOficina");
+            final String direccion = getText(document, "lblDireccion");
+
+            // Create a new persona.
+            final Persona persona = Persona.builder()
+                    .key(id)
+                    .nombre(nombre)
+                    .cargo(cargo)
+                    .unidad(unidad)
+                    .email(email)
+                    .telefonoFijo(telefono)
+                    .oficina(oficina)
+                    .build();
+
+            // Insert into the list
+            personas.add(persona);
 
         }
 
+        log.debug("Personas founded: {}", personas.size());
 
+        log.debug("Saving in json ..");
+
+        // Gson to use to convert List<Persona> to JSON.
+        final Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        // Write in a file
+        try (final Writer writer = Files.newBufferedWriter(Paths.get("personas.json"))) {
+            gson.toJson(personas, writer);
+        }
+
+        log.debug("Done.");
+
+    }
+
+    /**
+     * @param document to use.
+     * @param id       to find.
+     * @return the value.
+     */
+    @Nullable
+    private static String getText(final Document document, final String id) {
+
+        final Element element = document.getElementById(id);
+        if (element == null) {
+            return null;
+        }
+
+        final String text = element.text();
+        if (StringUtils.isEmpty(text)) {
+            return null;
+        }
+
+        final String value = StringUtils.trim(text);
+        log.debug("{} -> {}", id, value);
+        return value;
+
+    }
+
+    /**
+     * Just wait.
+     */
+    private static void sleep() {
+        try {
+            Thread.sleep(1000 + RandomUtils.nextInt(0, 1001));
+        } catch (InterruptedException e) {
+            // Nothing here
+        }
     }
 
 }
